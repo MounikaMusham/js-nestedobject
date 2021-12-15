4b9ea39180e16f94bff8ca3e783d0d954f0fa3d6
mounika <mounikamusham12@gmail.com>
2021-12-15 18:26:40 +0530

    nestedobject

---
+++ nestedobject.js
@@ -0,0 +1,27 @@
+var student ={
+    name:"mounika",
+    Age:15,
+    marks:{
+        science:75,
+        maths:90,
+        physics:80
+    }
+}
+console.log(student.Age)
+console.log(student.marks.maths)
+console.log(student.marks)
+console.log(typeof student)
+//
+var car ={
+    model:"Baleno",
+    owner:"xyz",
+    features:{
+        mileage:15,
+        fuel:"petrol",
+        airbags:"4"
+    }
+}
+console.log(car.owner);
+console.log(car.features.fuel);
+console.log(car.features);
+
